5683915abeab3f45bb8907c903bb9f876029f518
Mukul Howale <howalemukul@gmail.com>
2024-09-08 22:00:54 +0530

    Added basic files

---
+++ src/main/java/com/example/AllChat/AllChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.AllChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllChatApplication.class, args);
+	}
+
+}
