08b2619be20156abbe1d2be356afc43e92ace785
Mukul Howale <howalemukul@gmail.com>
2024-12-18 22:06:32 +0530

    update

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ http://localhost:8088
 service-registry
 http://localhost:8761
 
+zipkin
+http://localhost:9411/zipkin/
+
 user-service
 http://localhost:8081
 
--- user-service/src/main/java/com/allchat/user_service/controller/UserController.java
+++ user-service/src/main/java/com/allchat/user_service/controller/UserController.java
@@ -4,6 +4,7 @@ import com.allchat.user_service.dto.UserResponseDto;
 import com.allchat.user_service.dto.UserRequestDto;
 import com.allchat.user_service.service.IUserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final IUserService userService;
